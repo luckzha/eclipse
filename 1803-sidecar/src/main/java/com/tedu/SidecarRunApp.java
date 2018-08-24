88f5584a7286e2988ea8e14f222098b7683569d2
luckzha <zhao_yanw@163.com>
2018-08-24 16:40:18 +0800

    第一次提交

---
+++ 1803-sidecar/src/main/java/com/tedu/SidecarRunApp.java
@@ -0,0 +1,15 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+
+@SpringBootApplication
+@EnableSidecar
+public class SidecarRunApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SidecarRunApp.class, args);
+	}
+
+}
